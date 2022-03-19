342ece640b15ee4b97df0c317d1221bf4900b62b
orlinskia <artorl95@gmail.com>
2022-03-19 12:17:29 +0100

    Detail

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Button, Card, Col, Collapse, Row} from "react-bootstrap";
+
 
 const Detail = () => {
+
+    const [data, setData] = useState({});
+
+    function Example() {
+        const [open, setOpen] = useState(false);
+
+        return (
+            <>
+                <Button
+                    variant={"link"}
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    Show seller info
+                </Button>
+                <Collapse in={open}>
+                    <div id="example-collapse-text">
+                        <Card body style={{width: '400px'}} className="mx-auto">
+                            <p>Name: {data.seller}</p>
+                            <p>Phone: {data.sellerPhone}</p>
+                        </Card>
+                    </div>
+                </Collapse>
+            </>
+        );
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('/adverts/4');
+            setData(response.data);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
-            
+            <Card className="text-center">
+                <Card.Header><h1>{data.title}</h1></Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        <Row>
+                            <Col xs={"auto"}>
+                                <img src={`${data.image}?t=${data.id}`} height="300px"/>
+                            </Col>
+                            <Col>
+                                <p>{data.description}</p>
+                                <p>Price: ${data.price}</p>
+                                <p>Negotiation possible: {data.canNegotiate ? "Yes" : "No"}</p>
+                                <Example/>
+
+
+                            </Col>
+                        </Row>
+                    </Card.Text>
+
+                </Card.Body>
+                <Card.Footer className="text-muted">Created
+                    on: {new Date(data.createdOn).toLocaleString()}</Card.Footer>
+            </Card>
+
+
         </div>
     );
 };
--- src/views/List.js
+++ src/views/List.js
@@ -6,37 +6,31 @@ const List = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async() => {
+        const fetchData = async () => {
             const response = await axios.get('/adverts/');
             setData(response.data);
         }
         fetchData();
-        }, []);
-
+    }, []);
 
 
     return (
         <div>
             <ListGroup>
                 {data.map((advert) => (
-                    <ListGroupItem action key={advert.id}>
-                        <Row>
-                            <Col>
-                        <img src={`${advert.image}?t=${advert.id}`}/>
-                            </Col>
-                            <Col>
-                        <h1>{advert.title}</h1>
-                        <p>{advert.description}</p>
-                        <p>Price: ${advert.price}</p>
-                                <p>Negotiation possible: {advert.canNegotiate ? "Yes" : "No"}</p>
-                        <h4>Seller info:</h4>
-                        <p>Name: {advert.seller}</p>
-                        <p>Phone: {advert.sellerPhone}</p>
-                            </Col>
-                        </Row>
+                        <ListGroupItem action key={advert.id}>
+                            <Row>
+                                <Col xs={"auto"} className="my-auto" ys={"auto"}>
+                                    <img src={`${advert.image}?t=${advert.id}`} height="120px"/>
+                                </Col>
+                                <Col className="my-3" ys={"auto"}>
+                                    <h1>{advert.title}</h1>
+                                    <p>Price: ${advert.price}</p>
+                                </Col>
+                            </Row>
 
 
-                    </ListGroupItem>
+                        </ListGroupItem>
                     )
                 )}
             </ListGroup>
