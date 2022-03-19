32d3d0f037e402477a68dedb9b2c5eb6744722f5
orlinskia <artorl95@gmail.com>
2022-03-19 13:52:18 +0100

    List - Search Form

--- src/views/List.js
+++ src/views/List.js
@@ -1,22 +1,58 @@
 import React, {useEffect, useState} from 'react';
-import {Col, ListGroup, ListGroupItem, Row} from "react-bootstrap";
+import {Button, Card, Col, Form, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 import axios from "axios";
 import {LinkContainer} from "react-router-bootstrap";
 
 const List = () => {
     const [data, setData] = useState([]);
+    const [titleSearch, setTitleSearch] = useState("");
+    const [searchForm, setSearchForm] = useState();
+
+    const handleTitleSearchChange = (e) =>
+    {
+        setTitleSearch(e.target.value);
+    }
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault()
+        setSearchForm(titleSearch);
+    }
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await axios.get('/adverts/');
+            const response = await axios.get('/adverts', {params: {title_like: searchForm}});
             setData(response.data);
         }
         fetchData();
-    }, []);
+    }, [searchForm]);
 
 
     return (
         <div>
+
+            <Card body className="my-3">
+                <Form onSubmit={handleSubmit}>
+                    <Row>
+                        <Col>
+                    <Form.Group className="mb-3" controlId="formTitle">
+                        <Form.Control
+                            value={titleSearch}
+                            type="text"
+                            placeholder="Search title..."
+                            onChange={handleTitleSearchChange}
+                        />
+                    </Form.Group>
+                        </Col>
+
+                        <Col>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Card>
             <ListGroup>
                 {data.map((advert) => (
                     <LinkContainer to={`/details/${advert.id}`} key={advert.id}>
